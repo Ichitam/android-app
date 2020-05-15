17181ac985e861840146d17dd0ad6e8169618621
SeniorZhai <developer.zhaitao@gmail.com>
2020-05-15 20:43:44 +0800

    Custom view group

--- app/src/main/java/one/mixin/android/widget/FlagLayout.kt
+++ app/src/main/java/one/mixin/android/widget/FlagLayout.kt
@@ -3,21 +3,23 @@ package one.mixin.android.widget
 import android.content.Context
 import android.util.AttributeSet
 import android.view.LayoutInflater
-import android.widget.LinearLayout
+import android.view.ViewGroup
 import androidx.core.view.isVisible
 import kotlinx.android.synthetic.main.view_flag.view.*
 import one.mixin.android.R
 import one.mixin.android.extension.dp
 import one.mixin.android.extension.translationY
+import kotlin.math.max
 
 class FlagLayout @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null,
     defStyle: Int = 0
-) : LinearLayout(context, attrs, defStyle) {
+) : ViewGroup(context, attrs, defStyle) {
     var bottomFlag = false
         set(value) {
             if (field != value) {
+                down_flag_layout.isVisible = value
                 field = value
                 update()
             }
@@ -50,7 +52,29 @@ class FlagLayout @JvmOverloads constructor(
 
     init {
         LayoutInflater.from(context).inflate(R.layout.view_flag, this, true)
-        orientation = VERTICAL
+    }
+
+    override fun onLayout(changed: Boolean, l: Int, t: Int, r: Int, b: Int) {
+        var bottom = measuredHeight - paddingBottom
+        for (i in childCount - 1 downTo 0) {
+            val child = getChildAt(i)
+            if (child.isVisible) {
+                child.layout(paddingStart, bottom - child.measuredHeight, paddingStart + child.measuredWidth, bottom)
+                bottom -= child.measuredHeight
+            }
+        }
+    }
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        measureChildren(widthMeasureSpec, heightMeasureSpec)
+        var width = 0
+        var height = 0
+        for (i in 0 until childCount) {
+            val child = getChildAt(i)
+            width = max(child.measuredWidth, width)
+            height += child.measuredHeight
+        }
+        setMeasuredDimension(width + paddingStart + paddingEnd, height + paddingTop + paddingBottom)
     }
 
     private fun show() {
